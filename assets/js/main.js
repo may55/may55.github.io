bb5fc10d2905c3e8b14a753822e7fd231be9e824
may55 <mayank.soni055@gmail.com>
2024-12-06 20:42:32 +0530

    latest changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
+      typeSpeed: 80,
       backSpeed: 50,
-      backDelay: 2000
+      backDelay: 600
     });
   }
 
